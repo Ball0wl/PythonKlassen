c4c874e78f79bf535e658a6e919e7e9c80a0e97d
Ball0wl <98036412+Ball0wl@users.noreply.github.com>
2022-08-31 08:49:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,36 @@ class BauplanAutoKlasse():
     """ Klasse zum Erstellen von Autos, dies ist ein hilftext.
     """
 
-    def __init__(self, marke, farbe, baujahr, kmstand, sitze):
+    def __init__(self, marke, farbe, baujahr, kmstand=0, sitze=4):
         self.marke = marke
         self.farbe = farbe
         self.baujahr = baujahr
         self.kmstand = kmstand
         self.sitze = sitze
+        self.dauergarage = 0
+
+    def hupen(self, anzahl=3):
+        print(anzahl * "tuuU")
+
+    def steht_garage(self, dauer):
+        print(self.marke, "steht in der Garage. ", dauer, "Minuten ")
+        self.dauergarage += dauer
+        print(self.marke, "Steht insgesamt seit: ", self.dauergarage, " Minuten")
 
 
-auto_audi = BauplanAutoKlasse("Audi", "rot", 1999, 143000, 5)
+auto_audi = BauplanAutoKlasse("Audi", "Rot", 1999, 143000, 5)
+auto_bmw = BauplanAutoKlasse("BMW", "Silber", 2009)
+auto_mercedes = BauplanAutoKlasse("Mercedes", "Schwarz", 2014, 4)
 
-print("Marke:", auto_audi.marke, "Farbe", auto_audi.farbe, "Baujahr", auto_audi.baujahr, auto_audi.kmstand, "km", "Sitze", auto_audi.sitze)
+auto_bmw.steht_garage(60)
+auto_audi.steht_garage(30)
+
+auto_bmw.hupen()
+auto_audi.hupen()
+
+print("Marke:", auto_audi.marke, "Farbe", auto_audi.farbe, "Baujahr", auto_audi.baujahr, auto_audi.kmstand, "km",
+      "Sitze", auto_audi.sitze)
+print("Marke:", auto_bmw.marke, "Farbe", auto_bmw.farbe, "Baujahr", auto_bmw.baujahr, auto_bmw.kmstand, "km", "Sitze",
+      auto_bmw.sitze)
+print("Marke:", auto_mercedes.marke, "Farbe", auto_mercedes.farbe, "Baujahr", auto_mercedes.baujahr, auto_mercedes.kmstand, "km", "Sitze",
+      auto_mercedes.sitze)
